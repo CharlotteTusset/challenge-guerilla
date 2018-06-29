eda1235a5f8f983387f49734bbcac4758236bde7
Bare-guillaume <bare.guillaume@gmail.com>
2018-06-29 08:44:43 +0200

    animation + js contact form

--- app.js
+++ app.js
@@ -19,5 +19,24 @@ btnDon2.addEventListener("click",myFunction);
 function myFunction() {
   alert("If you feel generous, you can give BeCode T-shirts to the Devs of this website :) ");
 }
+
+
+
+
+let submit = document.getElementById("buttonSub");
+submit.addEventListener("click",()=>{
+  let first = document.getElementById("first").value;
+  let last = document.getElementById("last").value;
+  let mail = document.getElementById("mail").value;
+  let phone = document.getElementById("phone").value;
+  let ROC = document.getElementById("ReasonOfContact").value;
+  let msg = document.getElementById("msg").value;
+  
+  alert('Thank you for your message '+first+' '+last+'\n'
+  +'You contacted us for '+ROC+'\n'+'You message is '+msg+'\n'+'Your phone number is '+phone+'\n'
+  +'We will get back to you at this adress: '+mail);
+});
+
+
   
 
